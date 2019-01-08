62fa541362795a2078d7f58ba1e0e2fb7b0fb18b
Sugar Light <sugar6400@gmail.com>
2019-01-08 18:21:15 +0900

    Initial commit

---
+++ src/src/main/java/io/sugar6400/admiral/Admiral.java
@@ -0,0 +1,10 @@
+package main.java.io.sugar6400.admiral;
+
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.io.ClassPathResource;
+
+public class Admiral {
+}
